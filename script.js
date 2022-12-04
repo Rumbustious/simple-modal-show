afb84df5c0db63ece5769b5c24777fb90a335e83
Rumbustious <99288580+Rumbustious@users.noreply.github.com>
2022-12-04 14:53:03 +0200

    Finished mouse functionality 🐭     - Link script file     - Fix style error     - Add click events

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+const showBtns = document.querySelectorAll('.show');
+const closeBtn = document.querySelector('.close');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+
+
+// open modal panel
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+// close modal panel
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+// when show button is clicked
+for(let i = 0; i < showBtns.length; i++)
+    showBtns[i].addEventListener('click', openModal);
+
+// when X "close" button is clicked
+closeBtn.addEventListener('click', closeModal);
+//when it is click on overlay
+overlay.addEventListener('click', closeModal);
